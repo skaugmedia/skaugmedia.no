fde1ebd38d58b6bbc69d3b7b02914982bc99a934
Nina Merethe Skaug <nina.merethe.94@gmail.com>
2023-02-23 23:25:57 +0100

    added images and text to Pent og Brukt page

--- src/Data/GraphicalDesign/PentOgBrukt/index.js
+++ src/Data/GraphicalDesign/PentOgBrukt/index.js
@@ -1,16 +1,29 @@
 import { oneLine } from "common-tags";
 import pentogbrukt_logo from "../../../Images/pentogbrukt_logo.jpg";
+import bussinesscardimg from "./Images/visittkort.jpg";
+import openinghoursimg from "./Images/openinghours.jpg";
+import openinghours1img from "./Images/openinghours_1.jpg";
+import signimg from "./Images/fasadeskilt.jpg";
+import sign1img from "./Images/fasadeskilt_1.jpg";
+import colorpalette from "./Images/fargepalett.jpg";
+import typography from "./Images/typografi.jpg";
+import logo from "./Images/logo.jpg";
+import logomockup from "./Images/logo_mockup.jpg";
+import webpage from "./Images/webside.jpg";
+import webpage1 from "./Images/webside_1.jpg";
 
 export default {
   id: "pent-og-brukt-as",
   title: "Pent og Brukt AS",
   date: "2021-11-01",
   ingress: oneLine`
-    text
+    Pent og Brukt AS er en liten og søt bruktbutikk som ligger på Kløfta, i Ullensaker kommune. Jeg fikk i oppgave å lage et enkelt og tidløst design til deres bedrift. 
+    Da veggene ble malt i fargen "Minty Breeze" før jeg fikk dette oppdraget, inspirerte fargen meg til å bruke nettopp denne fargen i det helhetlige designet, for å knytte 
+    sammen alle grafiske elementer, sammen med butikkens innside. 
   `,
   body: oneLine`
-    adsfgchjvgcfdxzfs<adDFGH
-    DSSFFGS
+    Pent og Brukt AS sitt design har en enkel fargepallett, to fonter inkludert- der den ene kun brukes i logoen, mens den andre brukes i forskjellige fontstiler, for å skape 
+    et typografisk hiearki. Designet inneholder også tre forskjellige logoer, som alle har flere av de samme elementene, men der de alle skiller seg litt fra hverandre. 
   `,
   cover: {
     url: pentogbrukt_logo,
@@ -18,27 +31,41 @@ export default {
   },
   images: [
     {
-      url: "https://imengine.public.prod.tun.infomaker.io/?uuid=854d5d4d-cbf3-41d8-a6da-087ade2c7f9e&function=cover&type=preview&source=false&q=75&width=1600&height=839",
-      title: "altext",
+      url: logomockup,
+      title: "Logo",
     },
     {
-      url: "https://www.gravatar.com/avatar/deabcb6140a4cad604de58990bc2094c?s=64&d=identicon&r=PG&f=1",
-      title: "altext",
+      url: webpage,
+      title: "Webside",
+    },
+    {
+      url: webpage1,
+      title: "Webside",
+    },
+    {
+      url: bussinesscardimg,
+      title: "Visittkort",
+    },
+    {
+      url: openinghoursimg,
+      title: "Åpningstider",
+    },
+    {
+      url: openinghours1img,
+      title: "Åpningstider",
+    },
+    {
+      url: signimg,
+      title: "Fasadeskilt",
+    },
+    {
+      url: sign1img,
+      title: "Fasadeskilt",
     },
   ],
   designElements: {
-    fonts:
-      "https://imengine.public.prod.tun.infomaker.io/?uuid=854d5d4d-cbf3-41d8-a6da-087ade2c7f9e&function=cover&type=preview&source=false&q=75&width=1600&height=839",
-    colorPalette:
-      "https://imengine.public.prod.tun.infomaker.io/?uuid=854d5d4d-cbf3-41d8-a6da-087ade2c7f9e&function=cover&type=preview&source=false&q=75&width=1600&height=839",
-    logo: "https://imengine.public.prod.tun.infomaker.io/?uuid=854d5d4d-cbf3-41d8-a6da-087ade2c7f9e&function=cover&type=preview&source=false&q=75&width=1600&height=839",
+    fonts: typography,
+    colorPalette: colorpalette,
+    logo: logo,
   },
-  graphicalElements: [
-    {
-      url: "https://imengine.public.prod.tun.infomaker.io/?uuid=854d5d4d-cbf3-41d8-a6da-087ade2c7f9e&function=cover&type=preview&source=false&q=75&width=1600&height=839",
-    },
-    {
-      url: "https://imengine.public.prod.tun.infomaker.io/?uuid=854d5d4d-cbf3-41d8-a6da-087ade2c7f9e&function=cover&type=preview&source=false&q=75&width=1600&height=839",
-    },
-  ],
 };
