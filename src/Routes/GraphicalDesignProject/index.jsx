d151199f32e7f34566510c99445a6975baa5bf70
Victor Nascimento Bakke <gipphe@gmail.com>
2023-02-24 12:09:18 +0100

    wip: graphhical design project route (#5)

--- src/Routes/GraphicalDesignProject/index.jsx
+++ src/Routes/GraphicalDesignProject/index.jsx
@@ -2,14 +2,54 @@ import { useLoaderData } from "react-router-dom";
 import GraphicalDesignHeading from "../../Components/Headings/DesignHeading";
 import { PageSection } from "../../Components/PageSection";
 import "react-image-gallery/styles/css/image-gallery.css";
+import classes from "./GraphicalDesignProject.module.css";
 import ImageGallery from "react-image-gallery";
+import { Image } from "/src/Components/Image";
 
 export function GraphicalDesignProject() {
   const { project } = useLoaderData();
+  const images = project.images.map((img) => ({ original: img.url }));
   return (
-    <PageSection>
+    <PageSection className={classes.container}>
       <GraphicalDesignHeading>{project.title}</GraphicalDesignHeading>
-      <ImageGallery items={project.images} />
+      <ImageGallery
+        items={images}
+        showThumbnails={false}
+        showPlayButton={false}
+        showBullets={true}
+      />
+      <p className={classes.ingress}>{project.ingress}</p>
+      <div className={classes.designElements}>
+        <Image
+          className={classes.designElement}
+          fit="contain"
+          src={project.designElements.fonts}
+          alt="Fonts"
+        />
+
+        <Image
+          className={classes.designElement}
+          fit="contain"
+          src={project.designElements.logo}
+          alt="Logo"
+        />
+      </div>
+      <div className={classes.colorPalette}>
+        Fargepalette{" "}
+        {project.designElements.colorPalette.map((color) => (
+          <div
+            className={classes.colorCard}
+            style={{ "background-color": color.hex }}
+          >
+            <ul className={classes.colorDetails}>
+              <li>Hex: {color.hex}</li>
+              <li>
+                RGB: {color.rgb.r} {color.rgb.g} {color.rgb.b}
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
     </PageSection>
   );
 }
