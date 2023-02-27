ba219ffd9443b2721fb05c55e17d87fc1b6b103f
Victor Nascimento Bakke <gipphe@gmail.com>
2023-02-27 20:38:38 +0100

    wip

--- src/Routes/Root/index.jsx
+++ src/Routes/Root/index.jsx
@@ -22,90 +22,79 @@ import instagram from "../../Images/instagram.png";
 import tiktok from "../../Images/tiktok.png";
 import etsy from "../../Images/etsy.png";
 import { Link } from "react-router-dom";
-
-function myFunction(x) {
-  x.classList.toggle("change");
-}
+import { useState } from "react";
 
 export function Root() {
+  const [showNav, toggleNav] = useState(false);
+
   return (
-    <div className="Root">
-      <header className="header">
-        <NavLink to={HomeRoute} className={classes.plainLink}>
-          <img className="logo" src={skaugmedia} alt="Skaug Media logo"></img>
-        </NavLink>
-        <nav className="header row">
-          <NavLink className="nav-link" to={HomeRoute}>
-            {HomePageName}
+    <>
+      <div className="Root">
+        <header className="header">
+          <NavLink to={HomeRoute} className={classes.plainLink}>
+            <img className="logo" src={skaugmedia} alt="Skaug Media logo"></img>
           </NavLink>
-          <NavLink className="nav-link" to={PhotographyRoute}>
-            {PhotographyPageName}
-          </NavLink>
-          <NavLink className="nav-link" to={GraphicalDesignRoute}>
-            {GraphicalDesignPageName}
-          </NavLink>
-          <NavLink className="nav-link" to={PricesRoute}>
-            {PricesPageName}
-          </NavLink>
-          <NavLink className="nav-link" to={AboutRoute}>
-            {AboutPageName}
-          </NavLink>
-          <NavLink className="nav-link" to={ContactRoute}>
-            {ContactPageName}
-          </NavLink>
-        </nav>
-      </header>
 
-      <div className="hamburger" onClick={myFunction}>
-        <div className="bar1"></div>
-        <div className="bar2"></div>
-        <div className="bar3"></div>
+          <div className="hamburger" onClick={toggleNav}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
+
+          <Nav
+            containerClassName={`header row ${classes.hideOnMobile}`}
+            linkClassName="nav-link"
+          />
+        </header>
+
+        <main>
+          <Outlet />
+        </main>
+
+        <footer>
+          <Nav containerClassName="footer-links" linkClassName="footer-link" />
+          <div className="footer-social">
+            <Link to="https://www.facebook.com/profile.php?id=100089990337611">
+              <img className="social" src={facebook} alt="Facebook"></img>
+            </Link>
+            <Link to="https://www.instagram.com/skaugmedia/">
+              <img className="social" src={instagram} alt="Instagram"></img>
+            </Link>
+            <Link to="https://www.tiktok.com/@skaugmedia?_t=8a92nGlyw7N&_r=1">
+              <img className="social" src={tiktok} alt="Tiktok"></img>
+            </Link>
+            <Link to="https://www.etsy.com/shop/SkaugMedia?ref=search_shop_redirect">
+              <img className="social" src={etsy} alt="Etsy"></img>
+            </Link>
+          </div>
+        </footer>
       </div>
+      <div className={classes.mobileNav}></div>
+    </>
+  );
+}
 
-      <main>
-        <Outlet />
-      </main>
-      <footer>
-        <div className="footer-links">
-          <NavLink className="footer-link" to={HomeRoute}>
-            {HomePageName}
-          </NavLink>
-
-          <NavLink className="footer-link" to={PhotographyRoute}>
-            {PhotographyPageName}
-          </NavLink>
-
-          <NavLink className="footer-link" to={GraphicalDesignRoute}>
-            {GraphicalDesignPageName}
-          </NavLink>
-
-          <NavLink className="footer-link" to={PricesRoute}>
-            {PricesPageName}
-          </NavLink>
-
-          <NavLink className="footer-link" to={AboutRoute}>
-            {AboutPageName}
-          </NavLink>
-
-          <NavLink className="footer-link" to={ContactRoute}>
-            {ContactPageName}
-          </NavLink>
-        </div>
-        <div className="footer-social">
-          <Link to="https://www.facebook.com/profile.php?id=100089990337611">
-            <img className="social" src={facebook} alt="Facebook"></img>
-          </Link>
-          <Link to="https://www.instagram.com/skaugmedia/">
-            <img className="social" src={instagram} alt="Instagram"></img>
-          </Link>
-          <Link to="https://www.tiktok.com/@skaugmedia?_t=8a92nGlyw7N&_r=1">
-            <img className="social" src={tiktok} alt="Tiktok"></img>
-          </Link>
-          <Link to="https://www.etsy.com/shop/SkaugMedia?ref=search_shop_redirect">
-            <img className="social" src={etsy} alt="Etsy"></img>
-          </Link>
-        </div>
-      </footer>
-    </div>
+function Nav({ containerClassName, linkClassName }) {
+  return (
+    <nav className={containerClassName}>
+      <NavLink className={linkClassName} to={HomeRoute}>
+        {HomePageName}
+      </NavLink>
+      <NavLink className={linkClassName} to={PhotographyRoute}>
+        {PhotographyPageName}
+      </NavLink>
+      <NavLink className={linkClassName} to={GraphicalDesignRoute}>
+        {GraphicalDesignPageName}
+      </NavLink>
+      <NavLink className={linkClassName} to={PricesRoute}>
+        {PricesPageName}
+      </NavLink>
+      <NavLink className={linkClassName} to={AboutRoute}>
+        {AboutPageName}
+      </NavLink>
+      <NavLink className={linkClassName} to={ContactRoute}>
+        {ContactPageName}
+      </NavLink>
+    </nav>
   );
 }
