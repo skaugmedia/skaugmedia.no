6ddff40a0c12ea83c73192e8300531cb03142ea2
Victor Nascimento Bakke <gipphe@gmail.com>
2023-03-18 20:15:44 +0100

    add vite-imagetools

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { imagetools } from "vite-imagetools";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    imagetools({
+      removeMetadata: true,
+    }),
+  ],
 });
