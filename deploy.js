a0e0235296419ca0da353dfb6c6c25ca43f46bdf
Victor Nascimento Bakke <gipphe@gmail.com>
2023-03-24 22:51:20 +0100

    specify env file in deploy script

--- deploy.js
+++ deploy.js
@@ -1,9 +1,11 @@
 import * as ftp from "basic-ftp";
-import "dotenv/config";
+import dotenv from "dotenv";
 import { readdir, stat } from "fs/promises";
 import { stdin as input, stdout as output } from "node:process";
 import * as readline from "node:readline/promises";
 import { basename, join } from "path";
+
+dotenv.config({ path: `${__dirname}/.env.local` });
 
 const dirSize = async (dir) => {
   const files = await readdir(dir, { withFileTypes: true });
