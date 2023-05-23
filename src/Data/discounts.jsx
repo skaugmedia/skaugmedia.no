a7180e92c11259f1567e7c5924d69d1cd0a9b2d2
Nina Merethe Skaug <nina.merethe.94@gmail.com>
2023-05-23 13:28:13 +0200

    new discount

--- src/Data/discounts.jsx
+++ src/Data/discounts.jsx
@@ -17,23 +17,8 @@ export const discounts = [
       </>
     ),
     link: `/${PricesRoute}#${ids.miniSessionMinisessions}`,
-    validFrom: localTime("27-04-2023 00:00"),
-    validTo: localTime("12-05-2023 00:00"),
-  },
-  {
-    title: "VALGFRI MINIFOTOGRAFERING",
-    price: kr(850),
-    description: (
-      <>
-        *Gjelder ikke nyfødtfotograferinger eller bryllupsfotografering, og kan
-        ikke kombineres med andre tilbud. <br />
-        Helgetillegg og kveldstillegg kommer i tillegg i prisen, samt kjøring
-        utover 30Km fra Jessheim.
-      </>
-    ),
-    link: `/${PricesRoute}#${ids.miniSessionMinisessions}`,
     validFrom: localTime("15-05-2023 00:00"),
-    validTo: localTime("01-07-2023 00:00"),
+    validTo: localTime("24-05-2023 00:00"),
   },
   {
     title: "HELGETILBUD",
@@ -49,6 +34,36 @@ export const discounts = [
     buttonText: "Book her",
     link: `/${ContactRoute}`,
     validFrom: localTime("12-05-2023 00:00"),
-    validTo: localTime("15-05-2023 00:00"),
+    validTo: localTime("24-05-2023 00:00"),
+  },
+  {
+    title: "ONSDAGSTILBUD",
+    price: "50% på fotografering",
+    description: (
+      <>
+        Tilbudet gjelder kun 24.05.23
+        <br />
+        *Gjelder ikke minifotograferinger
+      </>
+    ),
+    buttonText: "Book her",
+    link: `/${ContactRoute}`,
+    validFrom: localTime("24-05-2023 00:00"),
+    validTo: localTime("25-05-2023 00:00"),
+  },
+  {
+    title: "VALGFRI MINIFOTOGRAFERING",
+    price: kr(850),
+    description: (
+      <>
+        *Gjelder ikke nyfødtfotograferinger eller bryllupsfotografering, og kan
+        ikke kombineres med andre tilbud. <br />
+        Helgetillegg og kveldstillegg kommer i tillegg i prisen, samt kjøring
+        utover 30Km fra Jessheim.
+      </>
+    ),
+    link: `/${PricesRoute}#${ids.miniSessionMinisessions}`,
+    validFrom: localTime("25-05-2023 00:00"),
+    validTo: localTime("01-07-2023 00:00"),
   },
 ];
