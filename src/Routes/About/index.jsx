be3216dde1772f23d367d19e71b315051afbc8b9
Nina Merethe Skaug <nina.merethe.94@gmail.com>
2023-08-27 21:29:27 +0200

    changed the whole About me page and added some new images

--- src/Routes/About/index.jsx
+++ src/Routes/About/index.jsx
@@ -1,33 +1,66 @@
 import FotografiHeading from "../../Components/Headings/FotografiHeading";
+import { PageSection } from "../../Components/PageSection";
 import { useDocumentTitle } from "../../Hooks/useDocumentTitle";
-import aboutnina from "../../Images/me.jpg";
-import classes from "./About.module.css";
+import AboutMe from "../../Images/meg.jpg";
+import "./About.css";
 import pageName from "./PageName";
-import { Image } from "/src/Components/Image";
-
 export function About() {
   useDocumentTitle(pageName);
   return (
-    <>
-      <div className={classes.background}>
-        <div className={classes.container}>
-          <FotografiHeading className={classes.heading}>
-            {pageName}
-          </FotografiHeading>
-          <Image
-            className={classes.headshot}
-            src={aboutnina}
-            alt="Nina Merethe Skaug"
-          />
-          <div className={classes.text}>
-            <Paragraphs />
+    <div className="AboutMe">
+      <PageSection
+        outerClassName="AboutMe__Outer"
+        innerClassName="AboutMe__Inner"
+      >
+        <FotografiHeading>Om meg</FotografiHeading>
+        <div className="AboutMe__Box">
+          <div className="AboutMe__Text">
+            <p>
+              Jeg heter Nina Merethe Skaug og jeg jobber som fotograf og
+              designer i Skaug Media. Jeg elsker å jobbe kreativt og jeg brenner
+              for å levere flotte bilder som du kan henge på veggen og som du
+              kan ha glede av i mange år fremover, om ikke for alltid!
+            </p>
+            <p>
+              Jeg bor på Jessheim i Ullensaker kommune, sammen med min forlovede
+              og en sønn på snart 2 år. Jeg tar på meg oppdrag på Romerike, Oslo
+              og omegn, men ta gjerne kontakt om du lurer på om du bor for langt
+              unna.
+            </p>
+            <p>
+              For meg er det viktig at dere skal ha en hyggelig opplevelse med å
+              være foran kamera, og tørre å være dere selv- da blir bildene og
+              opplevelsen best! Jeg gjør det jeg kan for at opplevelsen blir
+              bra, og jeg ønsker å fange de magiske øyeblikkene.
+            </p>
+            <p>
+              Jeg har ikke eget studio, da jeg alltid fotograferer på location,
+              og aller helst utendørs. Jeg vil alltid hjelpe til med veiledning
+              i forhold til valg av location og hva dere skal ha på dere under
+              fotograferingen. For gravide, nyfødt og babyer har jeg en liten
+              kundegarderobe som kan brukes ved fotografering. Om det skulle
+              være noe dere lurer på, så er ingen spørsmål dumme, og jeg svarer
+              så godt jeg kan på hva enn det måtte være!
+            </p>
+            <p>
+              I all hovedsak fotograferer jeg konfirmanter, baby, barn, familie,
+              portretter, produkter og matfotografi. I høst skal jeg starte opp
+              med eiendomsfotografering, og bruke en del av tiden min på dette.
+              Jeg fotograferer også for næringsliv og bedrifter, samt at jeg
+              leverer ulike designtjenester til både privatpersoner og
+              næringsliv. Jeg tilbyr både fulle designpakker og enkle
+              designtjenester, alt etter hva slags behov og ønsker dere har.
+            </p>
           </div>
-          <div className={classes.topLightgreen} />
-          <div className={classes.rightLightgreen} />
-          <div className={classes.bottomRightLightgreen} />
+
+          <img
+            className="AboutMe__Img"
+            src={AboutMe}
+            alt="Nina Merethe Skaug"
+          ></img>
         </div>
-      </div>
-    </>
+      </PageSection>
+    </div>
   );
 }
 
