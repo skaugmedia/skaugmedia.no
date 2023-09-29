1cca5d04c61f186eed6ae63f911917815c94a57a
Victor Nascimento Bakke <gipphe@gmail.com>
2023-09-30 00:20:30 +0200

    Change header to new design

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -4,38 +4,46 @@ import "./Header.css";
 import { Nav } from "../Nav";
 import { useState } from "react";
 import classNames from "classnames";
-import skaugmedia from "../../Images/skaugmedia.png";
+import skaugmedia from "../../Images/skaugmedia_logo_only.png";
 import { Hamburger } from "../Hamburger";
 
 export const Header = ({ isMobile }) => {
   const [showNav, setShowNav] = useState(false);
   return (
     <header className="Header">
-      <PlainLink to={HomeRoute} className="Header__LogoContainer">
-        {isMobile ? (
-          <h1 className="Header--Mobile">
-            <span className="Header--Mobile__First">Skaug</span>
-            <span className="Header--Mobile__Second">Media</span>
-          </h1>
-        ) : (
-          <img
-            className="Header__Logo"
-            src={skaugmedia}
-            alt="Skaug Media logo"
-          />
-        )}
-      </PlainLink>
+      <div className="Header__Inner">
+        <Hamburger
+          className={classNames("Header__Hamburger")}
+          toggled={showNav}
+          onClick={() => setShowNav(!showNav)}
+        />
 
-      <Hamburger
-        className={classNames("Header__Hamburger")}
-        toggled={showNav}
-        onClick={() => setShowNav(!showNav)}
-      />
+        <Nav
+          containerClassName="Header--Mobile__Nav"
+          linkClassName="Header--Mobile__NavLink"
+        />
 
-      <Nav
-        containerClassName="Header--Mobile__Nav"
-        linkClassName="Header--Mobile__NavLink"
-      />
+        <PlainLink to={HomeRoute} className="Header__LogoContainer">
+          {isMobile ? (
+            <h1 className="Header--Mobile">
+              <span className="Header--Mobile__First">Skaug</span>
+              <span className="Header--Mobile__Second">Media</span>
+            </h1>
+          ) : (
+            <>
+              <h1 className="Header__Name">
+                <span className="Header__NameFirst">Skaug</span>
+                <span className="Header__NameSecond">Media</span>
+              </h1>
+              <img
+                className="Header__Logo"
+                src={skaugmedia}
+                alt="Skaug Media logo"
+              />
+            </>
+          )}
+        </PlainLink>
+      </div>
     </header>
   );
 };
