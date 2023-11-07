5f96fb564a995070820e72a61a84e3c15d03bb29
Victor Nascimento Bakke <gipphe@gmail.com>
2023-11-07 23:38:27 +0100

    Fix contact page

--- src/Routes/Contact/index.jsx
+++ src/Routes/Contact/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import classNames from "classnames";
 import Button from "../../Components/Button";
 import DesignHeading from "../../Components/Headings/DesignHeading";
 import { PageSection } from "../../Components/PageSection";
@@ -7,6 +8,7 @@ import "./Contact.css";
 import pageName from "./PageName";
 
 import Contact_Img from "../../Images/Contact/LO_2.jpg";
+import { Input } from "../../Components/Input";
 
 export function Contact() {
   useDocumentTitle(pageName);
@@ -19,7 +21,7 @@ export function Contact() {
   const isInvalidMessage = queries.has("invalid:message");
   const isInvalidToken = queries.has("invaid:token");
   const isTokenExpiredOrDuplicate = queries.has(
-    "invalid:token-expired-or-duplicate"
+    "invalid:token-expired-or-duplicate",
   );
   const resultMessage = (() => {
     switch (true) {
@@ -50,118 +52,97 @@ export function Contact() {
     } catch (e) {}
   });
 
+  const results = showResults ? (
+    <div
+      className={classNames("Contact__Result", {
+        "Contact__Result--Error": isErr,
+      })}
+    >
+      {resultMessage}
+    </div>
+  ) : (
+    ""
+  );
+
   return (
-    <>
-      <PageSection className="container relative">
-        <div className="Contact__RowBox">
-          <div className="Contact__ImgBox">
-            <img
-              className="Contact__Img"
-              src={Contact_Img}
-              alt="Bilde av et ettårig barn"
-            ></img>
-          </div>
-          <div className="Contact__FormBox">
-            <form className="form" action="/send_mail.php" method="POST">
-              <div className="contact">
-                <div className="contact-content">
-                  <DesignHeading className="contact-heading">
-                    {pageName}
-                  </DesignHeading>
-                </div>
-                <div className="contact-text">
-                  Har du spørsmål til meg eller ønsker å booke fotografering? Ta
-                  gjerne kontakt med meg gjennom dette skjemaet, og skriv litt
-                  om hva henvendelsen gjelder i meldingsfeltet.
-                </div>
-              </div>
-              <label htmlFor="name" className="form-titles">
-                Navn
-              </label>
-
-              <input
-                className="form-box name"
-                placeholder="Navn Navnesen"
-                type="text"
-                id="name"
-                name="name"
-                minLength={2}
-                title="navn"
-                required
-              />
-
-              <label htmlFor="phone" className="form-titles">
-                Telefonnummer
-              </label>
-
-              <input
-                className="form-box phonenumber"
-                placeholder="12345678"
-                type="tel"
-                id="phonenumber"
-                name="phone"
-                pattern="((\+|00)47)?[24689][0-9]{7}"
-                title="telefonnummer"
-                required
-              />
-
-              <label htmlFor="email" className="form-titles">
-                E-post
-              </label>
-
-              <input
-                className="form-box email"
-                placeholder="navn.navnesen@epost.com"
-                type="text"
-                id="email"
-                name="email"
-                pattern="\S+@\S+\.\S+"
-                title="e-post"
-                required
-              />
-
-              <label htmlFor="message" className="form-titles">
-                Melding
-              </label>
-
-              <textarea
-                className="form-box message"
-                placeholder="Skriv din tekst her"
-                id="message"
-                name="message"
-                minLength={3}
-                required
-              />
-
-              <div
-                ref={reCaptcha}
-                className="g-recaptcha"
-                data-sitekey={import.meta.env.VITE_RECAPTCHA_KEY}
-              />
-
-              <input
-                type="hidden"
-                name="callback"
-                value="https://www.ninaskaug.no/kontakt"
-              />
-
-              <Button className="send-button" type="submit">
-                Send
-              </Button>
-
-              {showResults && (
-                <div
-                  className={`contact-result ${
-                    isErr ? "contact-result-error" : ""
-                  }`}
-                >
-                  {resultMessage}
-                </div>
-              )}
-            </form>
-          </div>
+    <PageSection>
+      <div className="Contact__RowBox">
+        <div className="Contact__ImgBox">
+          <img
+            className="Contact__Img"
+            src={Contact_Img}
+            alt="Bilde av et ettårig barn"
+          ></img>
         </div>
-      </PageSection>
-    </>
+        <div className="Contact__FormBox">
+          <form className="Contact__Form" action="/send_mail.php" method="POST">
+            <DesignHeading className="Contact__Heading">
+              {pageName}
+            </DesignHeading>
+            <div className="Contact__Text">
+              Har du spørsmål til meg eller ønsker å booke fotografering? Ta
+              gjerne kontakt med meg gjennom dette skjemaet, og skriv litt om
+              hva henvendelsen gjelder i meldingsfeltet.
+            </div>
+
+            <Input
+              name="name"
+              label="Navn"
+              required
+              minLength={2}
+              type="text"
+              placeholder="Navn Navnesen"
+              title="navn"
+            />
+
+            <Input
+              name="phone"
+              label="Telefonnummer"
+              type="tel"
+              pattern="((\+|00)47)?[24689][0-9]{7}"
+              placeholder="12345678"
+              title="telefonnummer"
+              required
+            />
+
+            <Input
+              label="E-post"
+              name="email"
+              type="text"
+              pattern="\S+@\S+\.\S+"
+              title="e-post"
+              required
+              placeholder="navn.navnesen@epost.com"
+            />
+
+            <Input
+              placeholder="Skriv din tekst her"
+              name="message"
+              label="Melding"
+              area={true}
+              minLength={3}
+              required
+            />
+
+            <div
+              ref={reCaptcha}
+              className="g-recaptcha"
+              data-sitekey={import.meta.env.VITE_RECAPTCHA_KEY}
+            />
+
+            <input
+              type="hidden"
+              name="callback"
+              value="https://www.ninaskaug.no/kontakt"
+            />
+
+            <Button className="Contact__SubmitButton" type="submit">
+              Send
+            </Button>
+            {results}
+          </form>
+        </div>
+      </div>
+    </PageSection>
   );
 }
