7f41449346877bc68272290b992d4f5a01d41463
Victor Nascimento Bakke <gipphe@gmail.com>
2024-02-18 22:49:24 +0100

    Add pageName to each route

--- src/router.jsx
+++ src/router.jsx
@@ -1,49 +1,67 @@
 import { createBrowserRouter } from "react-router-dom";
 import { About } from "./Routes/About";
+import AboutPageName from "./Routes/About/PageName";
 import AboutRoute from "./Routes/About/Route";
 import { BusinessPrices } from "./Routes/BusinessPrices";
+import BusinessPricesPageName from "./Routes/BusinessPrices/PageName";
 import BusinessPricesRoute from "./Routes/BusinessPrices/Route";
 import { Children } from "./Routes/Children";
+import ChildrenPageName from "./Routes/Children/PageName";
 import ChildrenRoute from "./Routes/Children/Route";
 import { Confirmation } from "./Routes/Confirmation";
+import ConfirmationPageName from "./Routes/Confirmation/PageName";
 import ConfirmationRoute from "./Routes/Confirmation/Route";
 import { Contact } from "./Routes/Contact";
+import ContactPageName from "./Routes/Contact/PageName";
 import ContactRoute from "./Routes/Contact/Route";
 import { Couples } from "./Routes/Couples";
+import CouplesPageName from "./Routes/Couples/PageName";
 import CouplesRoute from "./Routes/Couples/Route";
 import { Discounts } from "./Routes/Discounts";
+import DiscountsPageName from "./Routes/Discounts/PageName";
 import DiscountsRoute from "./Routes/Discounts/Route";
 import { ErrorHandler } from "./Routes/ErrorHandler";
 import { Family } from "./Routes/Family";
+import FamilyPageName from "./Routes/Family/PageName";
 import FamilyRoute from "./Routes/Family/Route";
 import { FirstYear } from "./Routes/FirstYear";
+import FirstYearPageName from "./Routes/FirstYear/PageName";
 import FirstYearRoute from "./Routes/FirstYear/Route";
 import { GraphicalDesign } from "./Routes/GraphicalDesign";
 import GraphicalDesignLoader from "./Routes/GraphicalDesign/Loader";
+import GraphicalDesignPageName from "./Routes/GraphicalDesign/PageName";
 import GraphicalDesignRoute from "./Routes/GraphicalDesign/Route";
 import { GraphicalDesignProject } from "./Routes/GraphicalDesignProject";
 import GraphicalDesignProjectLoader from "./Routes/GraphicalDesignProject/Loader";
 import GraphicalDesignProjectRoute from "./Routes/GraphicalDesignProject/Route";
 import { Headshots } from "./Routes/Headshots";
+import HeadshotsPageName from "./Routes/Headshots/PageName";
 import HeadshotsRoute from "./Routes/Headshots/Route";
 import { Home } from "./Routes/Home";
+import HomePageName from "./Routes/Home/PageName";
 import HomeRoute from "./Routes/Home/Route";
 import { Maternity } from "./Routes/Maternity";
+import MaternityPageName from "./Routes/Maternity/PageName";
 import MaternityRoute from "./Routes/Maternity/Route";
 import { Newborn } from "./Routes/Newborn";
+import NewbornPageName from "./Routes/Newborn/PageName";
 import NewbornRoute from "./Routes/Newborn/Route";
 import { PhotoProducts } from "./Routes/PhotoProducts";
+import PhotoProductsPageName from "./Routes/PhotoProducts/PageName";
 import PhotoProductsRoute from "./Routes/PhotoProducts/Route";
 import { Photography } from "./Routes/Photography";
 import PhotographyLoader from "./Routes/Photography/Loader";
+import PhotographyPageName from "./Routes/Photography/PageName";
 import PhotographyRoute from "./Routes/Photography/Route";
 import { PhotographyCategory } from "./Routes/PhotographyCategory";
 import PhotographyCategoryLoader from "./Routes/PhotographyCategory/Loader";
 import PhotographyCategoryRoute from "./Routes/PhotographyCategory/Route";
 import { PricesServices } from "./Routes/PricesServices";
+import PricesPageName from "./Routes/PricesServices/PageName";
 import PricesRoute from "./Routes/PricesServices/Route";
 import { Root } from "./Routes/Root";
 import { Wedding } from "./Routes/Wedding";
+import WeddingPageName from "./Routes/Wedding/PageName";
 import WeddingRoute from "./Routes/Wedding/Route";
 
 export const router = createBrowserRouter([
@@ -51,59 +69,124 @@ export const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorHandler />,
+    handle: {},
     children: [
       {
         children: [
-          { index: true, element: <Home /> },
-          { path: HomeRoute, element: <Home /> },
-          { path: AboutRoute, element: <About /> },
-          { path: ContactRoute, element: <Contact /> },
+          {
+            index: true,
+            element: <Home />,
+            handle: { pageName: HomePageName },
+          },
+          {
+            path: HomeRoute,
+            element: <Home />,
+            handle: { pageName: HomePageName },
+          },
+          {
+            path: AboutRoute,
+            element: <About />,
+            handle: { pageName: AboutPageName },
+          },
+          {
+            path: ContactRoute,
+            element: <Contact />,
+            handle: { pageName: ContactPageName },
+          },
           {
             path: `${BusinessPricesRoute}/${GraphicalDesignRoute}`,
             loader: GraphicalDesignLoader,
             element: <GraphicalDesign />,
+            handle: { pageName: GraphicalDesignPageName },
           },
           {
             path: `${BusinessPricesRoute}/${GraphicalDesignProjectRoute}`,
             loader: GraphicalDesignProjectLoader,
             element: <GraphicalDesignProject />,
+            handle: { pageName: (data) => data.title },
           },
           {
             path: PhotographyRoute,
             loader: PhotographyLoader,
             element: <Photography />,
+            handle: { pageName: PhotographyPageName },
           },
           {
             path: PhotographyCategoryRoute,
             loader: PhotographyCategoryLoader(false),
             element: <PhotographyCategory />,
+            handle: { pageName: (data) => data.title },
           },
-          { path: PricesRoute, element: <PricesServices /> },
+          {
+            path: PricesRoute,
+            element: <PricesServices />,
+            handle: { pageName: PricesPageName },
+          },
           {
             path: `${PricesRoute}/${WeddingRoute}`,
             element: <Wedding />,
+            handle: { pageName: WeddingPageName },
           },
           {
             path: `${BusinessPricesRoute}/:categoryId`,
             loader: PhotographyCategoryLoader(true),
             element: <PhotographyCategory />,
+            handle: { pageName: (data) => data.title },
           },
-          { path: DiscountsRoute, element: <Discounts /> },
-          { path: BusinessPricesRoute, element: <BusinessPrices /> },
-          { path: `${PricesRoute}/${NewbornRoute}`, element: <Newborn /> },
-          { path: `${PricesRoute}/${MaternityRoute}`, element: <Maternity /> },
-          { path: `${PricesRoute}/${ChildrenRoute}`, element: <Children /> },
-          { path: `${PricesRoute}/${FirstYearRoute}`, element: <FirstYear /> },
+          {
+            path: DiscountsRoute,
+            element: <Discounts />,
+            handle: { pageName: DiscountsPageName },
+          },
+          {
+            path: BusinessPricesRoute,
+            element: <BusinessPrices />,
+            handle: { pageName: BusinessPricesPageName },
+          },
+          {
+            path: `${PricesRoute}/${NewbornRoute}`,
+            element: <Newborn />,
+            handle: { pageName: NewbornPageName },
+          },
+          {
+            path: `${PricesRoute}/${MaternityRoute}`,
+            element: <Maternity />,
+            handle: { pageName: MaternityPageName },
+          },
+          {
+            path: `${PricesRoute}/${ChildrenRoute}`,
+            element: <Children />,
+            handle: { pageName: ChildrenPageName },
+          },
+          {
+            path: `${PricesRoute}/${FirstYearRoute}`,
+            element: <FirstYear />,
+            handle: { pageName: FirstYearPageName },
+          },
           {
             path: `${PricesRoute}/${ConfirmationRoute}`,
             element: <Confirmation />,
+            handle: { pageName: ConfirmationPageName },
           },
-          { path: `${PricesRoute}/${HeadshotsRoute}`, element: <Headshots /> },
-          { path: `${PricesRoute}/${FamilyRoute}`, element: <Family /> },
-          { path: `${PricesRoute}/${CouplesRoute}`, element: <Couples /> },
+          {
+            path: `${PricesRoute}/${HeadshotsRoute}`,
+            element: <Headshots />,
+            handle: { pageName: HeadshotsPageName },
+          },
+          {
+            path: `${PricesRoute}/${FamilyRoute}`,
+            element: <Family />,
+            handle: { pageName: FamilyPageName },
+          },
+          {
+            path: `${PricesRoute}/${CouplesRoute}`,
+            element: <Couples />,
+            handle: { pageName: CouplesPageName },
+          },
           {
             path: `${PricesRoute}/${PhotoProductsRoute}`,
             element: <PhotoProducts />,
+            handle: { pageName: PhotoProductsPageName },
           },
         ],
       },
