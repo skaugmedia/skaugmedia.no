71be858f8c98eb58cd07ec75d3996d11371aec58
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-03 21:20:18 +0100

    Use manual chunking

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import { imagetools } from "vite-imagetools";
 
 export default defineConfig({
@@ -9,4 +9,31 @@ export default defineConfig({
       removeMetadata: true,
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          const chunks = [
+            "react-collapsible",
+            "react-image-gallery",
+            "react-photo-album",
+            "react-responsive",
+            "react-router-dom",
+            "react",
+            "fortawesome",
+          ];
+
+          for (const chunk of chunks) {
+            if (id.includes(chunk)) {
+              return chunk;
+            }
+          }
+
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
