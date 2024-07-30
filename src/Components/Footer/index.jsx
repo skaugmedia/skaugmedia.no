6f588a4d8d54a14e767da6ec6945436c9d881b8c
Nina Merethe Skaug <nina.merethe.94@gmail.com>
2024-07-30 22:20:56 +0200

    made some changes to the footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,5 +1,5 @@
-import { SocialIcons } from "../SocialIcons";
 import { Nav } from "../Nav";
+import { SocialIcons } from "../SocialIcons";
 import "./Footer.css";
 
 import FooterLogo from "../../Images/Footer/skaug_media_simple_logo.png";
@@ -8,22 +8,34 @@ export const Footer = () => (
   <footer>
     <div className="Footer">
       <div className="Footer__ContentBox">
-        <Nav containerClassName="Footer__Links" linkClassName="Footer__Link" />
-        <div className="Footer__RowTwo">
-          <img
-            className="Footer__RowLogo"
-            src={FooterLogo}
-            alt="Skaug Media simple logo"
-          />
-          <div className="Footer__RowText">Nina Merethe Skaug</div>
-          <div className="Footer__RowText">Tlf. 464 18 241</div>
-          <div className="Footer__RowText">kontakt@skaugmedia.no</div>
-
-          <div className="Footer__SocialIcons">
-            <SocialIcons
-              containerClassName="Footer__Social"
-              itemClassName="Footer__SocialImg"
+        <div className="Footer__ColumnBox">
+          <div className="Footer__ColumnTwo">
+            <Nav
+              containerClassName="Footer__Links"
+              linkClassName="Footer__Link"
             />
+          </div>
+          <div className="Footer__ColumnOne">
+            <div className="Footer__ColumnText">Nina Merethe Skaug</div>
+            <div className="Footer__ColumnText">Tlf. 464 18 241</div>
+            <div className="Footer__ColumnText">
+              E-post: kontakt@skaugmedia.no
+            </div>
+            <div className="Footer__ColumnText">
+              Organisasjonsnummer: 930 561 673
+            </div>
+            <img
+              className="Footer__ColumnLogo"
+              src={FooterLogo}
+              alt="Skaug Media simple logo"
+            />
+
+            <div className="Footer__SocialIcons">
+              <SocialIcons
+                containerClassName="Footer__Social"
+                itemClassName="Footer__SocialImg"
+              />
+            </div>
           </div>
         </div>
       </div>
